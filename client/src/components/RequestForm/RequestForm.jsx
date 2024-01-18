55dc69581891a957a4e14add607631b5ce50d444
John Wage <john.m.wage@gmail.com>
2024-01-17 20:39:03 -0500

    Simplify React components

--- client/src/components/RequestForm/RequestForm.jsx
+++ client/src/components/RequestForm/RequestForm.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './RequestForm.css';
-import InputBar from '../InputBar/InputBar.jsx';
 
 const RequestForm = () => {
   return (
     <form>
-      <InputBar />
+      <select>
+        <option value='GET'>GET</option>
+        <option value='POST'>POST</option>
+        <option value='PUT'>PUT</option>
+        <option value='PATCH'>PATCH</option>
+        <option value='DELETE'>DELETE</option>
+      </select>
+
+      <input
+        className='Input'
+        type='text'
+        name='urlInput'
+        placeholder='https://API.com'
+        required
+      />
+
+      <button className='SendButton' name='SendButton' type='submit'>
+        Send
+      </button>
     </form>
   );
 };
