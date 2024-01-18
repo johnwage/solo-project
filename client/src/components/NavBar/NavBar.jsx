93e0e8f4a22b34e64948807d97af218721481d5b
John Wage <john.m.wage@gmail.com>
2024-01-18 12:25:11 -0500

    Style NavBar, including logo, shapes, and icons. Import fontAwesome library for icons

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHouse,
+  faClipboardCheck,
+  faCalendar,
+  faBox,
+  faChartSimple,
+  faUser,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   return (
     <div className='NavBar'>
+      <div className='topCircle'></div>
+      <div className='bottomCircle'></div>
+      <div className='yellowSemiCircle'></div>
+      <div className='blueSemiCircle'></div>
+
       <nav>
+        <div className='logo'>
+          <div className='circleLeft'></div>
+          <div className='circleRight'></div>
+          <div className='semiCircleLeft'></div>
+          <div className='semiCircleRight'></div>
+        </div>
+        {/* <div className='homeSelected'></div> */}
+
         <ul>
           <li>
             <img className='logo' src='' alt='Logo' />
@@ -12,27 +35,67 @@ const NavBar = () => {
 
         <ul className='menu'>
           <li>
-            <a className='homeSelected' href='/'>
-              Home
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faHouse}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
             </a>
           </li>
           <li>
-            <a href='/'>Board</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faClipboardCheck}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
           <li>
-            <a href='/'>Calendar</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faCalendar}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
           <li>
-            <a href='/'>Grid</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faBox}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
           <li>
-            <a href='/'>Graph</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faChartSimple}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
           <li>
-            <a href='/'>People</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faUser}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
           <li>
-            <a href='/'>Logout</a>
+            <a href='/'>
+              <FontAwesomeIcon
+                icon={faArrowRightFromBracket}
+                size='xl'
+                style={{ color: '#ffffff' }}
+              />
+            </a>
           </li>
         </ul>
       </nav>
